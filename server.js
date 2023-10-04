b48a273cbe944045f8045794cbcf2e77aa79b05d
CoCoSol007 <solois.corentin@gmail.com>
2023-10-04 17:10:26 +0200

    server update

--- server.js
+++ server.js
@@ -1,23 +1,19 @@
 const express = require('express');
-const cors = require('cors');
-const fs = require('fs');
 const app = express();
-app.use(cors());
 const path = require('path');
 
+const port = 80;
 
 // get web page  (http://localhost:3000/?name=FilePath)
 app.get('/', (req, res) => {
   paramValue = req.query.file;
   if (paramValue == undefined ){
-    paramValue = "main.html"
+    paramValue = "main.html";
   } 
   const filePath = path.join(__dirname, 'webpage', paramValue );
   res.sendFile(filePath);
 })
 
-const port = 3000;
-
 app.listen(port, () => {
-    console.log(`Serveur en cours d'exécution sur le port ${port}`);
+    console.log(`Serveur en cours d'exécution sur le port ${port} on ip: 0.0.0.0`);
 });
