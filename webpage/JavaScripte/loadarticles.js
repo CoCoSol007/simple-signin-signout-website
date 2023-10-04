6c22be3c87b2a912e7be65d23e6b1e3984069743
CoCoSol007 <solois.corentin@gmail.com>
2023-10-04 16:54:30 +0200

    button generator

---
+++ webpage/JavaScripte/loadarticles.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("/?file=data.json").then(response => response.json()).then(jsonData => {
+    
+    const buttonsContainer = document.getElementById("buttons-container");
+
+    // Parcours du tableau JSON et création des boutons
+    jsonData.forEach(item => {
+        const button = document.createElement("button");
+        const text = document.createElement("h3");
+        text.textContent = item.title;
+        button.appendChild(text);
+
+        button.addEventListener("click", function () {
+            // Redirection vers le fichier HTML en utilisant l'ID
+            window.location.href = "/?file=articles/" + item.id + ".html";
+        });
+
+        button.appendChild(document.createTextNode(item.autor));
+        buttonsContainer.appendChild(button);
+    });
+})
+});
+
+
+    
